6be4207136bb9db3cea6c6dee55deef5a16cbc89
Nathan Hazout <nathanh@il.ibm.com>
2015-07-08 13:29:13 +0300

    initial commit

---
+++ apps/LDAPLoginModule/common/js/main.js
@@ -0,0 +1,34 @@
+/*
+ * COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, modify, and distribute
+ * these sample programs in any form without payment to IBM® for the purposes of developing, using, marketing or distributing
+ * application programs conforming to the application programming interface for the operating platform for which the sample code is written.
+ * Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS AND IBM DISCLAIMS ALL WARRANTIES,
+ * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE SAMPLE SOURCE CODE.
+ * IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
+ */
+function wlCommonInit(){
+	
+}
+
+function getSecretData(){	
+	var resourceRequest = new WLResourceRequest("/adapters/DummyAdapter/getSecretData", WLResourceRequest.GET);
+	resourceRequest.send().then(
+			getSecretDataSuccess,
+			getSecretDataFailure
+	);
+}
+
+function getSecretDataSuccess(response){
+	$("#resultDiv").css("padding", "10px");
+	$("#resultDiv").html(new Date() + "<hr/>");
+	$("#resultDiv").append("Secret data :: " + response.responseJSON.secretData + "<hr/>");	
+	$("#resultDiv").append("Response :: " + JSON.stringify(response));
+}
+
+function getSecretDataFailure(response){
+	$("#resultDiv").css("padding", "10px");
+	$("#resultDiv").html(new Date() + "<hr/>");
+	$("#resultDiv").append("Error: " + JSON.stringify(response));
+}
